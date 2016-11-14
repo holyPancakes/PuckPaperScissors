6af18252aff1a78319dfa5c8dd6f217a13c2b625
Leo-Aldo Amante Delos Reyes <shishi.skywire.dr@gmail.com>
2016-11-14 16:50:22 +0800

    Add files via upload

--- ProjectClient.java
+++ ProjectClient.java
@@ -1,13 +1,37 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Scanner;
 
 //java GreetingClient 127.0.0.1 8080 username
 
-public class ProjectClient {
+public class ProjectClient extends JFrame {
 	
 	public static void main(String [] args) {
 		
+		//1. Create the frame.
+		JFrame frame = new JFrame("Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(600,600));
+		frame.setLayout(new BorderLayout());
+
+		JTextArea textArea = new JTextArea("");
+		textArea.setBackground(Color.YELLOW);
+
+		JScrollPane textAreaScroll = new JScrollPane(textArea);
+
+		textAreaScroll.setBounds(10,60,50,50);
+		textAreaScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		textAreaScroll.setSize(300,300);
+
+		frame.getContentPane().add(textAreaScroll);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+
 		try {
 			String serverName = args[0];
 			int port = Integer.parseInt(args[1]);   
@@ -38,6 +62,7 @@ public class ProjectClient {
 				catch(Exception e) { }
 				
 				out.writeUTF(username+" > " +message);
+				textArea.append(username + ">" + message +"\n");
 				if(message.equals("/quit")){
 					connected = false;
 				}
